bab61cdb1c9d9f628e939a6e165ac37ae137114b
axnish27 <aanish2710@gmail.com>
2023-12-06 18:03:07 +0530

    check mate coded

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-require_relative 'lib/peice'
+require_relative 'lib/piece'
 require_relative 'lib/board'
 
 
@@ -19,8 +19,8 @@ class Game
   def initialize(player1,player2)
     @board = Board.new(player1,player2)
     @play_board = @board.board
-    @find_co_ordinates = @board.hash
-    @captured_peices = []
+    @find_co_ordinates = @board.hashy
+    @captured_pieces = []
     @first_move = false
     @player_turn = "black"
     @check = false
@@ -40,18 +40,18 @@ class Game
 
   def turn
     input = gets.chomp.split(",")
-    move_peice(input[0],input[1])
+    move_piece(input[0],input[1])
 
   end
 
-  def available_moves()
+  def available_moves(start)
     start = start.split("?")[0]
     start_square = return_square(start)
-    peice = start_square.peice
-    peice.board = @play_board
-    peice.all_moves
-    available_moves = peice.available_moves.flatten.uniq
-    return puts available_moves
+    piece = start_square.piece
+    piece.board = @play_board
+    piece.all_moves
+    available_moves = !piece.available_moves.nil? ? piece.available_moves.flatten.uniq : nil
+    return p available_moves
   end
 
   def check_input?(start,destination=nil)
@@ -59,18 +59,18 @@ class Game
       puts "Input Move"
       return false
     elsif destination.nil? && start.include?("?") && in_board?(start.split("?")[0])
-      puts available_moves()
+      available_moves(start)
       return false
     elsif !in_board?(start) || !in_board?(destination)
       puts "invalid move"
       return false
     elsif return_square(start).nil?
-      puts "no peice at the square "
+      puts "no piece at the square "
       return false
-    elsif @first_move && return_square(start).peice.color == "black"
+    elsif @first_move && return_square(start).piece.color == "black"
       puts "white moves first"
       return false
-    elsif return_square(start).peice.color != @player_turn
+    elsif return_square(start).piece.color != @player_turn
       puts "#{@player_turn}'s moves"
       return false
     end
@@ -85,18 +85,18 @@ class Game
 
     opponent = @player_turn  == "white" ? "black" : "white"
 
-    opponent_peices = @play_board.flatten.find_all do |square|
-      if !square.peice.nil?
-        square.peice.color == opponent
+    opponent_pieces = @play_board.flatten.find_all do |square|
+      if !square.piece.nil?
+        square.piece.color == opponent
       end
     end
 
-    opponent_peices.each do |square|
-      peice = square.peice
-      peice.board = @play_board
-      peice.all_moves
-      if !peice.can_capture.compact.empty?
-        stil = peice.can_capture.any? {|move| move.peice.name == "King"}
+    opponent_pieces.each do |square|
+      piece = square.piece
+      piece.board = @play_board
+      piece.all_moves
+      if !piece.can_capture.compact.empty?
+        stil = piece.can_capture.any? {|move| move.piece.name == "King"}
         return stil if stil
       end
     end
@@ -106,16 +106,16 @@ class Game
   def is_check?(destination)
 
     start_square = return_square(destination)
-    peice = start_square.peice
-    peice.board = @play_board
-    peice.all_moves
-    peice.can_capture
-    check = peice.can_capture.any?{|move| move.peice.name == "King"}
+    piece = start_square.piece
+    piece.board = @play_board
+    piece.all_moves
+    piece.can_capture
+    check = piece.can_capture.any?{|move| move.piece.name == "King"}
     check
 
   end
 
-  def move_peice(start,destination=nil)
+  def move_piece(start,destination=nil)
 
     # while
     #   turn
@@ -124,29 +124,29 @@ class Game
 
     start_square = return_square(start)
     destination_square = return_square(destination)
-    peice = start_square.peice
+    piece = start_square.piece
 
 
 
-    peice.destination = @find_co_ordinates[destination]
-    peice.board = @play_board
+    piece.destination = @find_co_ordinates[destination]
+    piece.board = @play_board
 
-    return puts"invalid move for the peice check" if !peice.check_valid?
+    return puts"invalid move for the piece check" if !piece.check_valid?
 
-    available_moves = peice.available_moves.flatten.uniq
+    available_moves = piece.available_moves.flatten.uniq
 
-    return puts"invalid move for the peice" if !available_moves.include?(destination)
+    return puts"invalid move for the piece" if !available_moves.include?(destination)
 
     #find a way to put all of this
-    p "a",@check
+
     if @check == true
-      return puts "still in check " if simulator_check(start_square,destination_square,peice)
+      return puts "still in check " if simulator_check(start_square,destination_square,piece)
 
     end
 
-    destination_square.peice = peice
-    start_square.peice = nil
-    destination_square.peice.current_position = destination_square.co_ordinate
+    destination_square.piece = piece
+    start_square.piece = nil
+    destination_square.piece.current_position = destination_square.co_ordinate
 
 
 
@@ -155,25 +155,23 @@ class Game
     #checking if checkmade
     if @check
 
-      player_peices = @play_board.flatten.find_all do |square|
-        if !square.peice.nil?
-          square.peice.color == @player
+      player_pieces = @play_board.flatten.find_all do |square|
+        if !square.piece.nil?
+          square.piece.color == @player
         end
       end
 
-      checkmate = true
-      player_peices.each do |square|
-        peice = square.peice
-        peice.board = @play_board
-        peice.all_moves.each do |destination|
-          checkmate = simulator_check(square,square,return_square(destination),peice)
-          break if !checkmate
+      @checkmate = true
+      player_pieces.each do |square|
+        piece = square.piece
+        piece.board = @play_board
+        piece.all_moves.each do |destination|
+          @checkmate = simulator_check(square,square,return_square(destination),piece)
+          break if !@checkmate
         end
-        puts(checkmate)
       end
-
     end
-
+    p "check mate ? #{@checkmate}"
 
     puts "moved"
 
@@ -181,21 +179,21 @@ class Game
 
   end
 
-  def simulator_check(start_square,destination_square,peice)
-    temp = destination_square.peice
-    temp2 = start_square.peice
-    temp3 = destination_square.peice.nil? ? nil : destination_square.peice.current_position
+  def simulator_check(start_square,destination_square,piece)
+    temp = destination_square.piece
+    temp2 = start_square.piece
+    temp3 = destination_square.piece.nil? ? nil : destination_square.piece.current_position
 
-    destination_square.peice = peice
-    start_square.peice = nil
-    destination_square.peice.current_position = destination_square.co_ordinate
+    destination_square.piece = piece
+    start_square.piece = nil
+    destination_square.piece.current_position = destination_square.co_ordinate
 
     check = still_check?()
 
-    destination_square.peice = temp
-      start_square.peice = temp2
-      if !destination_square.peice.nil?
-        destination_square.peice.current_position = temp3
+    destination_square.piece = temp
+      start_square.piece = temp2
+      if !destination_square.piece.nil?
+        destination_square.piece.current_position = temp3
       end
 
     if check
@@ -215,35 +213,34 @@ class Game
 
 
   def delete_this()
+    # Assuming @play_board is a 2D array representing the chessboard
 
-    # square1 = @play_board[1][4]
-    # square2 = @play_board[5][0]
-    # square3 = @play_board[2][3]
-    # square4 = @play_board[0][3]
-    # square5 = @play_board[0][5]
+    # Set up the initial board
 
-    # square1.peice = nil
-    # square4.peice = nil
-    # square5.peice = nil
-
-    # knightbish1 = Rook.new("player2")
-    # knightbish1.color = "black"
-    # square2.peice = knightbish1
-    # knightbish1.current_position = square2.co_ordinate
-
-    # knightbish2 = Rook.new("player1")
-    # knightbish2.color = "white"
-    # square3.peice = knightbish2
-    # knightbish2.current_position = square2.co_ordinate
-
-    @play_board[1][4].piece = nil
-    @play_board[3][4].piece = nil
-    @play_board[1][3].piece = nil
-    @play_board[3][3].piece = nil
-
-    #try fools mate if check mate u got it
+    @play_board[1][5].piece = nil
+    @play_board[1][6].piece = nil
+    @play_board[6][4].piece = nil
 
 
+    # White moves
+    pawn1 = Pawn.new("player1")
+    pawn1.color = "white"
+    @play_board[3][5].piece = pawn1
+    pawn1.current_position = @play_board[3][5].co_ordinate
+
+    # Black makes a mistake
+    pawn2 = Pawn.new("player1")
+    pawn2.color = "white"
+    @play_board[3][6].piece = pawn2
+    pawn2.current_position = @play_board[3][6].co_ordinate
+
+    # White moves again
+
+    # Black makes another mistake (Fool's Mate)
+    pawn3 = Pawn.new("player2")
+    pawn3.color = "black"
+    @play_board[2][5].piece = pawn3
+    pawn3.current_position = @play_board[2][5].co_ordinate
   end
 
 
